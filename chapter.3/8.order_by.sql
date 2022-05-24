aced9a3807a2247c398ed2302a8069f259b41ed6
hanao870 <hanackholehole@gmail.com>
2022-05-24 11:26:40 +0000

    [update] order by 句の降順表示

--- chapter.3/8.order_by.sql
+++ chapter.3/8.order_by.sql
@@ -7,3 +7,14 @@ from
     shohin
 order by
     hanbai_tanka;
+
+-- 降順に並び替え
+select
+    shohin_id,
+    shohin_mei,
+    hanbai_tanka,
+    shiire_tanka
+from
+    shohin
+order by
+    hanbai_tanka desc;
