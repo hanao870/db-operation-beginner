ab33eb11774be6a2e9c776b2514737e565cbd688
hanao870 <hanackholehole@gmail.com>
2022-05-24 11:33:26 +0000

    [update] null を含む order by のソート
    null は比較演算子が使えないので、先頭か末尾にまとめて表示される

--- chapter.3/8.order_by.sql
+++ chapter.3/8.order_by.sql
@@ -30,3 +30,15 @@ from
 order by
     hanbai_tanka,
     shohin_id;
+
+-- null を含むソート
+-- null は比較演算子が使えないので、先頭か末尾にまとめて表示される
+select
+    shohin_id,
+    shohin_mei,
+    hanbai_tanka,
+    shiire_tanka
+from
+    shohin
+order by
+    shiire_tanka;
