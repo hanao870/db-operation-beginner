a4a3acda7fa8c7fd2108c3fa4daf2a75dec27dfe
hanao870 <hanackholehole@gmail.com>
2022-05-24 11:54:50 +0000

    [add] 第3章の練習問題回答
    3.1 の回答追加

---
+++ chapter.3/exercises/answer.sql
@@ -0,0 +1,13 @@
+-- 以下の3つの間違いがある
+-- 1. 文字の列を sum に指定している
+-- 2. where が group by より後になっている
+-- 3. select にない列名が group by に指定されている
+select
+    shohin_id,
+    sum(shohin_mei)
+from
+    shohin
+group by
+    shohin_bunrui
+where
+    torokubi > '2009-09-01';
