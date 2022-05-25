45bae4237980d91c2e2c7dc86129f3d0e664ebad
hanao870 <hanackholehole@gmail.com>
2022-05-25 12:14:30 +0000

    [update] テーブルのデータコピー

--- chapter.4/1.shohin_ins.sql
+++ chapter.4/1.shohin_ins.sql
@@ -215,3 +215,32 @@ values
         500,
         '2009-12-12'
     );
+
+-- 動作確認のために ShohinCopy テーブル削除
+drop table ShohinCopy;
+
+-- コピー先テーブルの作成
+create table ShohinCopy (
+    shohin_id char(4) not null,
+    shohin_mei varchar(100) not null,
+    shohin_bunrui varchar(32) not null,
+    hanbai_tanka integer,
+    shiire_tanka integer,
+    torokubi date,
+    primary key (shohin_id)
+);
+
+-- Shohin テーブルをコピー
+insert into
+    ShohinCopy (
+        shohin_id,
+        shohin_mei,
+        shohin_bunrui,
+        hanbai_tanka,
+        shiire_tanka,
+        torokubi
+    )
+select
+    *
+from
+    shohin;
