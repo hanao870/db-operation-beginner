2000d74bef03cb86b4e2b6c5246d0e089e6fcf92
hanao870 <hanackholehole@gmail.com>
2022-05-26 11:30:20 +0000

    [update] rollback 動作確認追加

--- chapter.4/4.transaction.sql
+++ chapter.4/4.transaction.sql
@@ -1,3 +1,8 @@
+select
+    *
+from
+    shohin;
+
 begin transaction;
 
 -- カッターシャツの販売単価を1000円値引き
@@ -17,3 +22,29 @@ where
     shohin_mei = 'Tシャツ';
 
 commit;
+
+-- 処理の取り消し rollback
+begin transaction;
+
+-- カッターシャツの販売単価を1000円値引き
+update
+    shohin
+set
+    hanbai_tanka = hanbai_tanka - 1000
+where
+    shohin_mei = 'カッターシャツ';
+
+-- Tシャツの販売単価を1000円値上げ
+update
+    shohin
+set
+    hanbai_tanka = hanbai_tanka + 1000
+where
+    shohin_mei = 'Tシャツ';
+
+rollback;
+
+select
+    *
+from
+    shohin;
