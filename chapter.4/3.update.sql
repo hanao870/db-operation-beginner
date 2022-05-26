b5379c4043111a48f85138c79c0b5b16670d5bd2
hanao870 <hanackholehole@gmail.com>
2022-05-26 09:50:23 +0000

    [update] where 句を用いた update

--- chapter.4/3.update.sql
+++ chapter.4/3.update.sql
@@ -1,5 +1,13 @@
--- torokubi を統一
+-- torokubi を更新. 全行が対象
 update
     shohin
 set
     torokubi = '2009-10-10';
+
+-- 条件を指定した update
+update
+    shohin
+set
+    hanbai_tanka = hanbai_tanka * 10
+where
+    shohin_bunrui = 'キッチン用品';
