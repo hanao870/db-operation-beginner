612dcd7b1a4acc987c3631ca7bd5b8e04d6f227a
hanao870 <hanackholehole@gmail.com>
2022-05-27 12:21:25 +0000

    [update] 多段ビューの SQL 追加
    view から view を作成する
    多段ビューはパフォーマンス低下を招くので非推奨

--- chapter.5/1.view.sql
+++ chapter.5/1.view.sql
@@ -15,3 +15,19 @@ from
     ShohinSum
 order by
     cnt_shohin;
+
+-- 多段ビューの作成
+-- 多段ビューはパフォーマンス低下を招くため非推奨
+create view ShohinSumJim (shohin_bunrui, cnt_shohin) as
+select
+    *
+from
+    ShohinSum
+where
+    shohin_bunrui = '事務用品';
+
+-- 多段ビューの表示
+select
+    *
+from
+    ShohinSumJim;
