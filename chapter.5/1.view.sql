b28b97ce7dc8cf370763f96229f4a170b3ba213b
hanao870 <hanackholehole@gmail.com>
2022-05-27 12:12:40 +0000

    [update] view を使用した select 文追加

--- chapter.5/1.view.sql
+++ chapter.5/1.view.sql
@@ -1,6 +1,5 @@
 -- Shohin テーブルの View 作成
-create view ShohinSum (shohin_bunrui, cnt_shohin) as
--- select 移行が view の本体となる
+create view ShohinSum (shohin_bunrui, cnt_shohin) as -- select 移行が view の本体となる
 select
     shohin_bunrui,
     count(*)
@@ -8,3 +7,9 @@ from
     Shohin
 group by
     shohin_bunrui;
+
+-- view を使用
+select
+    *
+from
+    ShohinSum;
