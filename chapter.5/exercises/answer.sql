aa3e063be55c2274b5d479fdb7f52e97a653f690
hanao870 <hanackholehole@gmail.com>
2022-05-28 07:11:21 +0000

    [add] 5章の練習問題 SQL 追加
    5.1 の回答追加

---
+++ chapter.5/exercises/answer.sql
@@ -0,0 +1,17 @@
+-- 5.1 の SQL
+create view ViewRenshu5_1 (shohin_mei, hanbai_tanka, torokubi) as
+select
+    shohin_mei,
+    hanbai_tanka,
+    torokubi
+from
+    Shohin
+where
+    hanbai_tanka >= 1000
+    and torokubi = '2009-09-20';
+
+-- 作成した View の表示
+select
+    *
+from
+    ViewRenshu5_1;
