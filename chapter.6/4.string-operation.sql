d4e4d7972a8522e0b9f57498b4e51272d92e4b0c
hanao870 <hanackholehole@gmail.com>
2022-05-29 08:13:00 +0000

    [add] 文字列の長さ LENGTH 関数追加

--- chapter.6/4.string-operation.sql
+++ chapter.6/4.string-operation.sql
@@ -13,3 +13,11 @@ select
     str1 || str2 || str3 as str_concat
 from
     SampleStr;
+
+-- 文字列の長さ
+-- PostgreSQL はマルチバイト文字も1文字としてカウントする
+select
+    str1,
+    length(str1) as len_str
+from
+    SampleStr;
