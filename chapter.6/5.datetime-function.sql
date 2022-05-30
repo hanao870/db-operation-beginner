127f81dec156de5682489947a6d75dd423f04a8e
hanao870 <hanackholehole@gmail.com>
2022-05-30 19:53:50 +0900

    [update] 日付要素切り出し EXTRACT 関数追加

--- chapter.6/5.datetime-function.sql
+++ chapter.6/5.datetime-function.sql
@@ -1,8 +1,45 @@
 -- 現在の日付を取得
-select CURRENT_DATE;
+select
+    CURRENT_DATE;
 
 -- 現在時刻を取得
-select CURRENT_TIME;
+select
+    CURRENT_TIME;
 
 -- 現在の日時を取得
-select CURRENT_TIMESTAMP;
+select
+    CURRENT_TIMESTAMP;
+
+-- 日付要素の切り出し
+select
+    CURRENT_TIMESTAMP,
+    EXTRACT(
+        year
+        from
+            CURRENT_TIMESTAMP
+    ) as year,
+    EXTRACT(
+        month
+        from
+            CURRENT_TIMESTAMP
+    ) as month,
+    EXTRACT(
+        day
+        from
+            CURRENT_TIMESTAMP
+    ) as day,
+    EXTRACT(
+        hour
+        from
+            CURRENT_TIMESTAMP
+    ) as hour,
+    EXTRACT(
+        minute
+        from
+            CURRENT_TIMESTAMP
+    ) as minute,
+    EXTRACT(
+        second
+        from
+            CURRENT_TIMESTAMP
+    ) as second;
