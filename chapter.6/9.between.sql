73055e3e453f95cfc5939bc09cb25b414401e7a5
hanao870 <hanackholehole@gmail.com>
2022-05-31 20:55:32 +0900

    [update] > と < による条件 SQL 追加
    between は条件値が含まれる

--- chapter.6/9.between.sql
+++ chapter.6/9.between.sql
@@ -7,3 +7,13 @@ from
 where
     hanbai_tanka between 100
     and 1000;
+
+-- 販売単価が 101 ~ 999 の商品を選択
+select
+    shohin_mei,
+    hanbai_tanka
+from
+    Shohin
+where
+    hanbai_tanka > 100
+    and hanbai_tanka < 1000;
