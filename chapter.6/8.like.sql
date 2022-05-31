cd5b0f37ab7118417e10d02022c59b8d746284d4
hanao870 <hanackholehole@gmail.com>
2022-05-31 20:50:51 +0900

    [update] like と _(アンダーバー)の後方一致 SQL 追加

--- chapter.6/8.like.sql
+++ chapter.6/8.like.sql
@@ -22,3 +22,19 @@ from
     SampleLike
 where
     strcol like '%ddd';
+
+-- like と _(アンダーバー)による後方一致
+select
+    *
+from
+    SampleLike
+where
+    -- _ は任意の 1 文字を意味する
+    strcol like 'abc__';
+
+select
+    *
+from
+    SampleLike
+where
+    strcol like 'abc___';
