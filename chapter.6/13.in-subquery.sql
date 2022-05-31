f4491eea6599e27107b2fbbe21bbdc57dea45f7b
hanao870 <hanackholehole@gmail.com>
2022-05-31 21:28:27 +0900

    [update] not in サブクエリを使用する SQL 追加

--- chapter.6/13.in-subquery.sql
+++ chapter.6/13.in-subquery.sql
@@ -1,3 +1,8 @@
+select
+    *
+from
+    TenpoShohin;
+
 -- 大阪に置いてある商品の販売単価を求める
 select
     shohin_id,
@@ -13,4 +18,21 @@ where
             TenpoShohin
         where
             tenpo_id = '000C'
+    );
+
+-- not in にサブクエリを使用
+select
+    shohin_id,
+    shohin_mei,
+    hanbai_tanka
+from
+    Shohin
+where
+    shohin_id not in (
+        select
+            shohin_id
+        from
+            TenpoShohin
+        where
+            tenpo_id = '000A'
     );
