3847f709b0acfc0e19c99ba71805b127c7e698ef
hanao870 <hanackholehole@gmail.com>
2022-06-01 18:41:59 +0900

    [update] not exists 動作確認 SQL 追加

--- chapter.6/14.exist.sql
+++ chapter.6/14.exist.sql
@@ -35,3 +35,21 @@ where
             TS.tenpo_id = '000C'
             and TS.shohin_id = S.shohin_id
     );
+
+-- 東京店に置いてある商品以外の販売単価
+select
+    shohin_id,
+    shohin_mei,
+    hanbai_tanka
+from
+    Shohin as S
+where
+    not EXISTS (
+        select
+            *
+        from
+            TenpoShohin as TS
+        where
+            TS.tenpo_id = '000A'
+            and TS.shohin_id = S.shohin_id
+    );
