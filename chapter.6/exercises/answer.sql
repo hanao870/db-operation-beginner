b8853bd64b5e49f62b914c76b48be0c11977fbd9
hanao870 <hanackholehole@gmail.com>
2022-06-01 20:20:23 +0900

    [update] 6章の練習問題 SQL 追加
    6.2 の SQL 追加

--- chapter.6/exercises/answer.sql
+++ chapter.6/exercises/answer.sql
@@ -18,4 +18,29 @@ select
 from
     Shohin
 where
+    -- not in に null が含まれると1行も選択されない!!
     shiire_tanka not in (500, 2800, 5000, null);
+
+-- 6.2
+select
+    count(
+        case
+            when hanbai_tanka <= 1000 then hanbai_tanka
+            else null
+        end
+    ) as low_price,
+    count(
+        case
+            when hanbai_tanka between 1001
+            and 3000 then hanbai_tanka
+            else null
+        end
+    ) as mid_price,
+    count(
+        case
+            when hanbai_tanka >= 3001 then hanbai_tanka
+            else null
+        end
+    ) as high_price
+from
+    Shohin;
