46f4d106bb8c8ba85e200a0c9ccb69949972f716
hanao870 <hanackholehole@gmail.com>
2022-06-01 19:01:07 +0900

    [update] 集約関数に case 式を含む SQL

--- chapter.6/6.3/15.case.sql
+++ chapter.6/6.3/15.case.sql
@@ -8,3 +8,35 @@ select
     end as abc_shohin_bunrui
 from
     Shohin;
+
+-- 商品分類毎の販売単価の合計
+select
+    shohin_bunrui,
+    sum(hanbai_tanka) as sum_tanka
+from
+    Shohin
+group by
+    shohin_bunrui;
+
+-- 商品分類毎に販売単価を合計した結果を行列変換する
+select
+    sum(
+        case
+            when shohin_bunrui = '衣服' then hanbai_tanka
+            else 0
+        end
+    ) as sum_tanka_ihuku,
+    sum(
+        case
+            when shohin_bunrui = 'キッチン用品' then hanbai_tanka
+            else 0
+        end
+    ) as sum_tanka_kitchen,
+    sum(
+        case
+            when shohin_bunrui = '事務用品' then hanbai_tanka
+            else 0
+        end
+    ) as sum_tanka_jimu
+from
+    shohin;
