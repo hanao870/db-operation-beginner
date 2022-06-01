3620b653d277bbaa67bc65b18f95a73af80df7a8
hanao870 <hanackholehole@gmail.com>
2022-06-01 18:38:25 +0900

    [update] exists 引数の select に定数を記述

--- chapter.6/14.exist.sql
+++ chapter.6/14.exist.sql
@@ -6,9 +6,29 @@ select
 from
     Shohin as S
 where
+    -- EXISTS の引数は常に相関サブクエリを指定する
     ExISTS(
         select
             *
+        from
+            TenpoShohin as TS
+        where
+            TS.tenpo_id = '000C'
+            and TS.shohin_id = S.shohin_id
+    );
+
+select
+    shohin_id,
+    shohin_mei,
+    hanbai_tanka
+from
+    Shohin as S
+where
+    ExISTS(
+        -- select には適当な定数を記述可能
+        -- 慣例的に select * を記述する
+        select
+            1
         from
             TenpoShohin as TS
         where
