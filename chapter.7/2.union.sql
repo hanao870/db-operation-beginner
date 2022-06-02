fafd3e9bf6f117a35247f821b36417b9c023d0f5
hanao870 <hanackholehole@gmail.com>
2022-06-02 18:45:20 +0900

    [update] union の列数違い SQL 追加
    union 対象の列数は同じ必要がある

--- chapter.7/2.union.sql
+++ chapter.7/2.union.sql
@@ -12,3 +12,19 @@ from
     Shohin2
 order by
     shohin_id;
+
+-- 演算対象のテーブルの列数は同じである
+select
+    shohin_id,
+    shohin_mei
+from
+    Shohin
+union
+select
+    shohin_id,
+    shohin_mei,
+    hanbai_tanka
+from
+    Shohin2
+order by
+    shohin_id;
