281a3b9b7d2ab3ac56c5f3817c1f7b3335438c41
hanao870 <hanackholehole@gmail.com>
2022-06-02 18:53:37 +0900

    [update] union の order by 句は1つのみ

--- chapter.7/2.union.sql
+++ chapter.7/2.union.sql
@@ -58,3 +58,22 @@ from
     Shohin2
 order by
     shohin_id;
+
+-- order by 句は最後に1つだけ
+select
+    shohin_id,
+    shohin_mei
+from
+    Shohin
+where
+    shohin_bunrui = 'キッチン用品'
+union
+select
+    shohin_id,
+    shohin_mei
+from
+    Shohin2
+where
+    shohin_bunrui = 'キッチン用品'
+order by
+    shohin_id;
