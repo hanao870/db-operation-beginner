b255c181f69318d56f7ac689d3efb0689368093e
hanao870 <hanackholehole@gmail.com>
2022-06-03 20:51:25 +0900

    [update] inner join を含む view 作成 SQL 追加

--- chapter.7/7.2/5.inner-join.sql
+++ chapter.7/7.2/5.inner-join.sql
@@ -10,3 +10,28 @@ from
     inner join Shohin as S on TS.shohin_id = S.shohin_id
 where
     TS.tenpo_id = '000A';
+
+drop view ShohinJoinTenpoShohin;
+
+-- 内部結合の view 作成
+create view ShohinJoinTenpoShohin (
+    tenpo_id,
+    tenpo_mei,
+    shohin_id,
+    shohin_mei,
+    hanbai_tanka
+) as
+select
+    TS.tenpo_id,
+    TS.tenpo_mei,
+    TS.shohin_id,
+    S.shohin_mei,
+    S.hanbai_tanka
+from
+    TenpoShohin as TS
+    inner join Shohin as S on TS.shohin_id = S.shohin_id;
+
+select
+    *
+from
+    ShohinJoinTenpoShohin;
