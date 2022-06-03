7cbb5c10adb9747e0320a304c9f30a636cba9e61
hanao870 <hanackholehole@gmail.com>
2022-06-03 20:41:13 +0900

    [add] inner join 動作確認 SQL

---
+++ chapter.7/7.2/5.inner-join.sql
@@ -0,0 +1,10 @@
+-- 2 つのテーブルを内部結合する
+select
+    TS.tenpo_id,
+    TS.tenpo_mei,
+    TS.shohin_id,
+    S.shohin_mei,
+    S.hanbai_tanka
+from
+    TenpoShohin as TS
+    inner join Shohin as S on TS.shohin_id = S.shohin_id;
