0c3a69666978c71e9907f9d97788b1ac8bb1860c
hanao870 <hanackholehole@gmail.com>
2022-06-04 21:52:49 +0900

    [update] 7章の練習問題 SQL
    7.2 の回答 SQL 追加. coalesce 関数を使用

--- chapter.7/exercises/answer.sql
+++ chapter.7/exercises/answer.sql
@@ -32,3 +32,15 @@ select
 from
     TenpoShohin as TS
     right outer join Shohin as S on TS.shohin_id = S.shohin_id;
+
+-- 7.2 の回答 SQL
+select
+    -- coalesce 関数は null を null 以外の引数の値に変換する
+    coalesce(TS.tenpo_id, '不明') as tenpo_id,
+    coalesce(TS.tenpo_mei, '不明') as tenpo_mei,
+    S.shohin_id,
+    S.shohin_mei,
+    S.hanbai_tanka
+from
+    TenpoShohin as TS
+    right outer join Shohin as S on TS.shohin_id = S.shohin_id;
