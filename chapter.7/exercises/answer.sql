7bd8447de6e055b277a698dafdc85c540f9243e2
hanao870 <hanackholehole@gmail.com>
2022-06-04 21:44:50 +0900

    [update] 7章の練習問題 SQL
    7.2 の SQL 追加

--- chapter.7/exercises/answer.sql
+++ chapter.7/exercises/answer.sql
@@ -15,3 +15,20 @@ from
     Shohin
 order by
     shohin_id;
+
+-- 7.2 の SQL
+select
+    case
+        when TS.tenpo_id is null then '不明'
+        else TS.tenpo_id
+    end,
+    case
+        when TS.tenpo_mei is null then '不明'
+        else TS.tenpo_mei
+    end,
+    S.shohin_id,
+    S.shohin_mei,
+    S.hanbai_tanka
+from
+    TenpoShohin as TS
+    right outer join Shohin as S on TS.shohin_id = S.shohin_id;
