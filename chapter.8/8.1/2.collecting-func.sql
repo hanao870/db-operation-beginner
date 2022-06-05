0f3d5634f30a161487a055febf98153e2318def3
hanao870 <hanackholehole@gmail.com>
2022-06-05 21:06:04 +0900

    [add] 集約関数をウィンドウ関数で使用 する SQL
    SUM のウィンドウ関数使用例追加

---
+++ chapter.8/8.1/2.collecting-func.sql
@@ -0,0 +1,11 @@
+-- sum 関数をウィンドウ関数として使う
+select
+    shohin_id,
+    shohin_mei,
+    hanbai_tanka,
+    sum(hanbai_tanka) over (
+        order by
+            shohin_id
+    ) as current_sum
+from
+    Shohin;
