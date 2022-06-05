d9b50f158664a8600a72afe6606e0d48e06d3d6f
hanao870 <hanackholehole@gmail.com>
2022-06-05 20:48:02 +0900

    [add] olap 関数動作確認 SQL
    ウィンドウ関数 rank 追加

---
+++ chapter.8/8.1/1.olap.sql
@@ -0,0 +1,15 @@
+select
+    shohin_id,
+    shohin_mei,
+    shohin_bunrui,
+    hanbai_tanka,
+    rank () over (
+        -- partition by : 順位を付ける対象の範囲を設定
+        -- partition by で区切られたレコードの集合をウィンドウと呼ぶ
+        partition by shohin_bunrui
+        -- order by : どの列を、どんな順位をつけるかを指定
+        order by
+            hanbai_tanka
+    ) as ranking
+from
+    Shohin;
