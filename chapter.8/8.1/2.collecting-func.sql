8da0bb0f62aa91ec9e1cff34fe47adf8193420ee
hanao870 <hanackholehole@gmail.com>
2022-06-05 21:08:14 +0900

    [update] AVG のウィンドウ関数 SQL 追加

--- chapter.8/8.1/2.collecting-func.sql
+++ chapter.8/8.1/2.collecting-func.sql
@@ -9,3 +9,19 @@ select
     ) as current_sum
 from
     Shohin;
+
+-- avg 関数をウィンドウ関数として使う
+select
+    shohin_id,
+    shohin_mei,
+    hanbai_tanka,
+    sum(hanbai_tanka) over (
+        order by
+            shohin_id
+    ) as current_sum,
+    avg(hanbai_tanka) over (
+        order by
+            shohin_id
+    ) as current_avg
+from
+    Shohin;
