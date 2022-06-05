bbbcde4cb9423037645745bda6e48430780a1bbd
hanao870 <hanackholehole@gmail.com>
2022-06-05 21:31:47 +0900

    [update] 集計対象を前後1行に指定する SQL 追加

--- chapter.8/8.1/3.frame.sql
+++ chapter.8/8.1/3.frame.sql
@@ -13,3 +13,37 @@ select
     ) as moving_avg
 from
     Shohin;
+
+-- 集計対象を3行後にする(エラーになる)
+select
+    shohin_id,
+    shohin_mei,
+    hanbai_tanka,
+    sum(hanbai_tanka) over (
+        order by
+            shohin_id rows 2 following
+    ) as moving_sum,
+    avg(hanbai_tanka) over (
+        order by
+            shohin_id rows 2 following
+    ) as moving_avg
+from
+    Shohin;
+
+-- カレントレコードの前後の行を集計対象に含める
+select
+    shohin_id,
+    shohin_mei,
+    hanbai_tanka,
+    sum(hanbai_tanka) over (
+        order by
+            shohin_id rows between 1 preceding
+            and 1 following
+    ) as moving_sum,
+    avg(hanbai_tanka) over (
+        order by
+            shohin_id rows between 1 preceding
+            and 1 following
+    ) as moving_avg
+from
+    Shohin;
