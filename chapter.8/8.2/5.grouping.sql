23fafce8329a64eaab46f925b3c37c632269c4a6
hanao870 <hanackholehole@gmail.com>
2022-06-07 21:31:35 +0900

    [update] 超集合の行に任意の文字列を登録

--- chapter.8/8.2/5.grouping.sql
+++ chapter.8/8.2/5.grouping.sql
@@ -7,3 +7,21 @@ from
     Shohin
 group by
     rollup(shohin_bunrui, torokubi);
+
+-- 超集合行のキーに適当な文字列を埋め込む
+select
+    case
+        when grouping(shohin_bunrui) = 1 then '商品分類 合計'
+        else shohin_bunrui
+    end as shohin_bunrui,
+    case
+        when grouping(torokubi) = 1 then '登録日 合計'
+        -- case によって文字列型と日付型が混在するので構文エラーとなる
+        -- そのため、日付型を文字列に変換する必要がある
+        else cast(torokubi as varchar(16))
+    end as torokubi,
+    sum(hanbai_tanka) as sum_tanka
+from
+    Shohin
+group by
+    rollup(shohin_bunrui, torokubi);
