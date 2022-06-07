27cdb93e511515575d3ffabaa9f764b1a8476eb1
hanao870 <hanackholehole@gmail.com>
2022-06-07 21:35:48 +0900

    [add] cube 動作確認 SQL

---
+++ chapter.8/8.2/6.cube.sql
@@ -0,0 +1,17 @@
+-- cube で可能な全ての組み合わせを取得
+select
+    case
+        when grouping(shohin_bunrui) = 1 then '商品分類 合計'
+        else shohin_bunrui
+    end as shohin_bunrui,
+    case
+        when grouping(torokubi) = 1 then '登録日 合計'
+        -- case によって文字列型と日付型が混在するので構文エラーとなる
+        -- そのため、日付型を文字列に変換する必要がある
+        else cast(torokubi as varchar(16))
+    end as torokubi,
+    sum(hanbai_tanka) as sum_tanka
+from
+    Shohin
+group by
+    cube(shohin_bunrui, torokubi);
